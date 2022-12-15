d07bbd78ba995bde86eaf3f06869741848a14de4
hailu-mengesha <hailemariam0303@gmail.com>
2022-12-15 11:17:48 -0800

    Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
+* main - checks function that prints if integer is positive or negative
+* Return: Alwayes 0
 */
 int main(void)
 {
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,8 +2,7 @@
 #define MAIN_H
 
 #include <stdio.h>
-void postitive_or_negative(int i);
-int largest_number(int a, int b, int c);
-void print_remaining_days(int month, int day, int year);
 
-#endif
+void positive_or_negative(int i);
+
+#endif /* MAIN_H */
