5a596097f5b54994ce1821bf9e6c66f35c1279c1
hailu-mengesha <hailemariam0303@gmail.com>
2022-12-16 04:36:24 -0800

    Multiple mains

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+/**
+ * main -prints positive or negative
+ * Return: Alwayes 0
+*/
+
+
+int main(void)
+
+{
+
+	int n;
+
+
+
+	srand(time(0));
+
+	n = rand() - RAND_MAX / 2;
+
+	/* your code goes there */
+
+	return (0);
+
+}
