b5726cd0aafd87007388c54178b443c88d3bca2b
hailu-mengesha <hailemariam0303@gmail.com>
2023-04-12 10:52:34 -0700

    djb2

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -28,4 +28,6 @@ typedef struct hash_table_s
 } hash_table_t;
 /*0-hash_table_create.c*/
 hash_table_t *hash_table_create(unsigned long int size);
+/*1-djb2.c*/
+unsigned long int hash_djb2(const unsigned char *str);
 #endif
